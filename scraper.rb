d44a343ab014ac67e39fc076f72d78595b43eb4d
Roberto Barros <roberto.f.barros@gmail.com>
2022-04-26 17:50:46 -0300

    livecode

---
+++ scraper.rb
@@ -0,0 +1,29 @@
+require 'open-uri'
+require 'nokogiri'
+
+def fetch_top_movie_urls
+  top_movies_url = 'https://www.imdb.com/chart/top'
+  html_file = URI.open(top_movies_url).read
+  html_doc = Nokogiri::HTML(html_file)
+
+  # TODO: return top movies URLs
+
+  links = []
+  html_doc.search('.titleColumn a').first(5).each do |link|
+    links << 'https://www.imdb.com' + link.attributes['href'].value
+  end
+
+  links
+end
+
+def scrape_movie(url)
+  html_file = URI.open(url, 'Accept-Language' => 'en').read
+  html_doc = Nokogiri::HTML(html_file)
+
+  # TODO: return movie info hash
+  {
+    title: html_doc.search('h1').text,
+    year: html_doc.search('.sc-8c396aa2-2')[0].text.to_i,
+    storyline: html_doc.search('.sc-16ede01-0').text
+  }
+end
